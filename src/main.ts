d2257535bcb8914e677cdc0d982e4f13b42f6323
Marek Wilk <marek.wilk@me.com>
2022-09-11 09:53:56 +0200

    Nest settings (prefix, helmet, port)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(helmet());
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.API_PORT ?? 3001);
 }
 bootstrap();
