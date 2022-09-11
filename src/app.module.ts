07ea6950707cae0f2d9092f56965ba2ef43eef52
Marek Wilk <marek.wilk@me.com>
2022-09-11 20:06:49 +0200

    Add services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DistanceService } from './distance/distance.service';
+import { StatsController } from './stats/stats.controller';
+import { TripsController } from './trips/trips.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, StatsController, TripsController],
+  providers: [AppService, DistanceService],
 })
 export class AppModule {}
