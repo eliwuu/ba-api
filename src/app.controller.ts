d00f8836e913953e7dc20d77609eff43a71fb91b
Marek Wilk <marek.wilk@me.com>
2022-09-11 20:06:14 +0200

    Test service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DistanceService } from './distance/distance.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly distanceService: DistanceService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('distance')
+  async getDistance(): Promise<string> {
+    return (
+      await this.distanceService.calculateByStreetName('ab,cd,ef', 'ab,cde,ef')
+    ).toString();
   }
 }
