c1d9745a24e417d44c56529644a1ee7b8cfe2aff
Marek Wilk <marek.wilk@me.com>
2022-09-11 20:06:42 +0200

    Initial stats controller

--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -3,8 +3,16 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('stats')
 export class StatsController {
   @Get('/weekly')
-  async GetWeekly(options?: { startDate: Date }) {}
+  async GetWeekly(options?: { startDate?: Date }) {
+    return new Promise((resolve, _reject) => {
+      resolve({ distance: '123km', total: '2342.90PLN' });
+    });
+  }
 
   @Get('monthly')
-  async GetMonthly(options?: { startDate: Date; stopDate: Date }) {}
+  async GetMonthly(options?: { startDate?: Date; stopDate?: Date }) {
+    return new Promise((resolve, _reject) => {
+      resolve({ distance: '123km', total: '2342.90PLN' });
+    });
+  }
 }
