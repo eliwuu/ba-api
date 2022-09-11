c2dce0058c01b6ccf95923b267a8ee9e57f32f30
Marek Wilk <marek.wilk@me.com>
2022-09-11 19:52:03 +0200

    Add initial test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DistanceService } from './distance/distance.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +9,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, DistanceService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -17,6 +18,9 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+    it('should return 250', () => {
+      expect(appController.getDistance()).toBe('250');
     });
   });
 });
